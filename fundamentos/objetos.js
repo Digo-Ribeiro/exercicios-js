c9fc57c49247a6504e639c530141fd0600b3736b
maschinedreams <gabrielle.clima23@gmail.com>
2019-11-22 18:30:17 -0300

    aula 28

--- fundamentos/objetos.js
+++
@@ -1,6 +0,0 @@
-const prod1 = {}
-prod1.nome = 'Celular Utra Mega' // propriedade do objeto
-prod1.preco = 4998.90 // propriedade do objeto 
-prod1['Desconto Legal'] = 0.40
-
-console.log(prod1)
--- fundamentos/organizaçao.js
+++
@@ -1,9 +0,0 @@
-console.log(
-    "sentença de código"
-)
-{
-    {
-        console.log("Olá");
-        console.log('Mundo') // padrão do curso
-    }
-}
