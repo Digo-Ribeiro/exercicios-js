52cc5a81b51abbf34481dd59eb2a58b1f63198a6
Rodrigo Ribeiro <rodrigo_mrclon@hotmail.com>
2020-07-04 23:47:06 -0300

    Update for.js

--- controle/for.js
+++ controle/for.js
@@ -1,4 +1,5 @@
-let contador = 1 
+let contador = 1
+
 while(contador <= 10) {
     console.log(`contador = ${contador}`);
     contador++
@@ -9,6 +10,7 @@ for(let i = 1; i <= 10; i++) {
 }
 
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
-for(i = 0; i < notas.length;  i++) {
+
+for(let i of notas){
     console.log(`nota = ${notas[i]}`)
 }
