9c8879b39febc81c73340ef9bcbbfdbca116d75d
Rodrigo Ribeiro <rodrigo_mrclon@hotmail.com>
2020-07-04 23:51:34 -0300

    Update if2.js

--- controle/if2.js
+++ controle/if2.js
@@ -15,5 +15,7 @@ console.log(num)
 }
 }
 
+//const teste2 = (num) => { let printNumber = (num>7) ? console.log(num) : false } --- Mesma função de cima, só que usando apenas uma linha e operador ternário!
+
 teste2(6)
 teste2(8)
